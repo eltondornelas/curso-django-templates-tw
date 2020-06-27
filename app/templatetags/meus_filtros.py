7fb0a61f4d65c073baf294046ecdfd5479dcfedb
Elton Dornelas <eltondornelas@gmail.com>
2020-06-27 08:29:04 -0300

    Separando o base.html em pequenos blocos de templates e incluindo nos templates, enviando variáveis no include

--- app/templatetags/meus_filtros.py
+++ app/templatetags/meus_filtros.py
@@ -1,10 +1,11 @@
 from django import template
 
 register = template.Library()
+
+
 # custom-template-tags -> djando docs
-
-
 # name= é o nome do filtro a ser utilizado no template
+
 @register.filter(name='remover_letra')
 def remover_letra(var, letra):
     return var.replace(letra, '')
